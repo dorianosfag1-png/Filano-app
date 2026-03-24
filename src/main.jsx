46932fdfc5fbd617fb46b2ac178b572a704de243
dorianosfag1-png <dorianosfag1@gmail.com>
2026-03-24 14:19:47 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Filano from './Filano'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><Filano /></React.StrictMode>
+)
