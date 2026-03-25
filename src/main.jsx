017992fcb4dccfd90d21baa3a135c7a0ac05e5b3
dorianosfag1-png <dorianosfag1@gmail.com>
2026-03-25 23:33:33 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Filano from './Filano'
+import Filano from './Filano.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><Filano /></React.StrictMode>
+  <React.StrictMode>
+    <Filano />
+  </React.StrictMode>
 )
